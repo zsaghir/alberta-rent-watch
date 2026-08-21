e871a6819a265f4f178f330b18511991e1df86cf
zsaghir <zsaghir@ualberta.ca>
2026-08-21 13:05:17 -0600

    created dashboard page

---
+++ app.py
@@ -0,0 +1,6 @@
+import streamlit as st
+# setting browser title
+st.set_page_config(page_title="Edmonton Rent Watch", page_icon=":house:")
+#main page heading
+st.title("Edmonton Rent Watch")
+st.caption("Edmonton two-bedroom purpose-built rental market")
