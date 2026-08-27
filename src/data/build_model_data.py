f9c4e254f43ec6beca30f5e2232b287511d35b5e
zsaghir <zsaghir@ualberta.ca>
2026-08-27 01:53:54 -0600

    added lag variables for model predicitng

--- src/data/build_model_data.py
+++ src/data/build_model_data.py
@@ -1,1 +1,18 @@
+import pandas as pd
 
+df = pd.read_csv("data/processed/edmonton_2br_master.csv")
+df = df.sort_values("year").reset_index(drop=True)
+df["rent_lag1"] = df["average_rent"].shift(1)
+df["vacancy_lag1"] = df["vacancy_rate"].shift(1)
+model_df = df.dropna(
+    subset=[
+        "rent_lag1",
+        "vacancy_lag1"
+    ]
+).copy()
+model_df.to_csv(
+    "data/model/edmonton_2br_model.csv",
+    index=False
+)
+print(model_df.head())
+print(f"Rows: {len(model_df)}")
